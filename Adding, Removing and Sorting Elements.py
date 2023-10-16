0de24e0f429109ea369c48a3f5d8f0a10a6c3f5e
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-16 15:54:24 +0600

    Add files via upload

---
+++ Adding, Removing and Sorting Elements.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+my_arr1 = np.array([10,2,30,23,1,2,3,4,5,6,7,8,9,0])
+my_arr2 = np.array([3,4,5,6])
+
+# sort:
+sort_my_array = np.sort(arr)
+print(sort_my_array)
+# concatenate:
+my_arr3 = np.concatenate((my_arr1, my_arr2))
+print(my_arr3)
+
+
