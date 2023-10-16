870c54652585485d943710c7d68b470326752214
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-16 15:48:30 +0600

    Add files via upload

---
+++ Adding linespace.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+np_array = np.linspace(0, 10, num=5)
+print(np_array)
+
--- lesson_1.py
+++ lesson_1.py
@@ -13,12 +13,17 @@ print(n4)
 # initializing numpy array with same number:
 n5 = np.full((2,2),10)
 print(n5)
+# create an empty arry:
+n_empty = np.empty(2)
+print(n_empty)
 # initializing numpy array within a range:
 n6 = np.arange(10,20)
 print(n6)
+# initializing numpy array with first number, last number and step size:
 n7 = np.arange(10,50,5)
 print(n7)
 # initializing numpy array with random numbers:
 n8 = np.random.randint(1,100,5)
 print(n8)
+
 
